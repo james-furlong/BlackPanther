2aadf9b5f8c231bd83d106bbf11f7945e91b7868
james-furlong <james-furlong@users.noreply.github.com>
2021-01-10 07:57:36 +1100

    Initial project commit

---
+++ BlackPanther/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BlackPanther
+//
+//  Created by James Furlong on 10/1/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
